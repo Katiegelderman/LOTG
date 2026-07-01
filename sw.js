146fe1df5536384866c273e8fc8284164960d820
Katie Gelderman <148262796+Katiegelderman@users.noreply.github.com>
2026-06-30 22:25:13 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,49 @@
-const CACHE_NAME = "spanish-trainer-v4";
-
-const FILES_TO_CACHE = [
+const CACHE_NAME = "lotg-v5";
+const APP_SHELL = [
   "/LOTG/",
   "/LOTG/index.html",
   "/LOTG/manifest.json",
-  "/LOTG/sw.js"
+  "/LOTG/sw.js",
+  "/LOTG/icons/icon-192.png",
+  "/LOTG/icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+          return networkResponse;
+        })
+        .catch(() => caches.match("/LOTG/index.html"));
     })
   );
 });
